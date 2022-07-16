cd30f613e254a024d232cdd8921dbb663d62d5d4
Ilya <bkmz181@gmail.com>
2022-07-16 12:33:46 +0300

    Init repo

---
+++ configure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+git submodule update --init --recursive || echo "error updating submodules"
+
+# build tgbot-cpp
+cd tgbot-cpp
+cmake .
+make -j4
+
+echo "Finished updating submodules"
